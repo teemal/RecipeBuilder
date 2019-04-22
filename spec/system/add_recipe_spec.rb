a7299ca694258094ff900e33d02c36057bdbb794
mwinb <mwinb>
2019-04-21 17:07:30 -0700

    Fixing Environment

---
+++ spec/system/add_recipe_spec.rb
@@ -0,0 +1,9 @@
+require "rails_helper"
+
+RSpec.describe "adding a recipe", type: :system do
+  it "allows a user to create a recipe with steps" do
+    visit new_recipe_path
+    fill_in "name"
+  end
+end
+
