04a9932111480f2d800b0c82e81a14a7b6912ab9
Myles Moylan <moylanm@oregonstate.edu>
2019-04-27 13:24:15 -0700

    creates recipe spec complete

---
+++ spec/workflows/creates_recipe_spec.rb
@@ -0,0 +1,58 @@
+require "rails_helper"
+
+RSpec.describe CreatesRecipe do
+
+  let(:creator) { CreatesRecipe.new(
+    name: "Pancakes",
+    ingredients_string: ingredients_string,
+    steps_string: steps_string
+  )}
+
+  describe "initialization" do
+    let(:ingredients_string) { "" }
+    let(:steps_string) { "" }
+
+    it "creates a recipe given a name" do
+      creator.build
+      expect(creator.recipe.name).to eq("Pancakes")
+    end
+
+  end
+
+  describe "string parsing" do
+    let(:ingredients) { creator.string_to_ingredients }
+    let(:steps) { creator.string_to_steps }
+
+    describe "handles empty strings" do
+      let(:ingredients_string) { "" }
+      let(:steps_string) { "" }
+      specify { expect(ingredients).to be_empty }
+      specify { expect(steps).to be_empty }
+    end
+
+    describe "with a single item" do
+      let(:ingredients_string) { "Stuff" }
+      let(:steps_string) { "Things" }
+      specify { expect(ingredients.first).to have_attributes(
+        ingredient: "Stuff"
+      )}
+      specify { expect(steps.first).to have_attributes(
+        step: "Things"
+      )}
+    end
+
+    describe "with multiple items" do
+      let(:ingredients_string) { "Salt\nPepper"}
+      let(:steps_string) { "Shake\nBake"}
+      specify { expect(ingredients).to match(
+        [an_object_having_attributes(ingredient: "Salt"),
+         an_object_having_attributes(ingredient: "Pepper")]
+      )}
+      specify { expect(steps).to match(
+        [an_object_having_attributes(step: "Shake"),
+         an_object_having_attributes(step: "Bake")]
+      )}
+    end
+  end
+
+end
