39973d84c15accc76880568f5fa39b85e4f69dad
Myles Moylan <moylanm@oregonstate.edu>
2019-04-27 13:24:43 -0700

    CreatesRecipe class working properly;

---
+++ app/workflows/creates_recipe.rb
@@ -0,0 +1,36 @@
+class CreatesRecipe
+  attr_accessor :name, :recipe, :steps_string, :ingredients_string
+
+  def initialize(name: "", ingredients_string: "", steps_string: "")
+    @name = name
+    @ingredients_string = ingredients_string
+    @steps_string = steps_string
+  end
+
+  def build
+    self.recipe = Recipe.new(
+      name: name,
+      ingredients: string_to_ingredients,
+      steps: string_to_steps
+    )
+    recipe
+  end
+
+  def create
+    build
+    recipe.save
+  end
+
+  def string_to_ingredients
+    @ingredients_string.split("\n").map do |ingredient|
+      Ingredient.new(ingredient: ingredient)
+    end
+  end
+
+  def string_to_steps
+    @steps_string.split("\n").map do |step|
+      Step.new(step: step)
+    end
+  end
+
+end
