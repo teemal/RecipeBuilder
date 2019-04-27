9705667c924ce11e13e1368046ac3e98dd22c917
Myles Moylan <moylanm@oregonstate.edu>
2019-04-27 13:37:18 -0700

    add create and index methods

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -2,4 +2,18 @@ class RecipesController < ApplicationController
   def new
     @recipe = Recipe.new
   end
+
+  def index
+    @recipes = Recipe.all
+  end
+
+  def create
+    @workflow = CreatesRecipe.new(
+      name: params[:recipe][:name],
+      ingredients_string: params[:recipe][:ingredients],
+      steps_string: params[:recipe][:steps]
+    )
+    @workflow.create
+    redirect_to recipes_path
+  end
 end
