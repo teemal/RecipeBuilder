bb965902019cb7faa90184e359c89ce56271df7c
Myles Moylan <moylanm@oregonstate.edu>
2019-04-27 16:22:38 -0700

    add validation, default values, and bool attr check method

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -1,4 +1,17 @@
 class Ingredient < ApplicationRecord
+  before_save :default_values
+  validates :name, presence: true
+
   belongs_to :recipe
 
+
+  def acquired?
+    acquired
+  end
+
+  private
+
+  def default_values
+    self.acquired = acquired.presence || false
+  end
 end
--- app/models/step.rb
+++ app/models/step.rb
@@ -1,4 +1,16 @@
 class Step < ApplicationRecord
+  before_save :default_values
+  validates :description, presence: true
+
   belongs_to :recipe
 
+  def completed?
+    completed
+  end
+
+  private
+
+  def default_values
+    self.completed = completed.presence || false
+  end
 end
