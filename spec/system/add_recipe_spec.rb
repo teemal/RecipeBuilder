d1d2032d70be0cf5903641ef4dce600844907132
Myles Moylan <moylanm@oregonstate.edu>
2019-04-27 13:37:56 -0700

    clean up add_recipe_spec

--- spec/system/add_recipe_spec.rb
+++ spec/system/add_recipe_spec.rb
@@ -1,15 +1,14 @@
 require "rails_helper"
 
 RSpec.describe "adding a recipe", type: :system do
-  it "allows a user to create a recipe with steps" do
+  it "allows a user to create a recipe with ingredients/steps" do
     visit new_recipe_path
-    fill_in "Name", with: "Test Pancakes"
-    fill_in "Ingredient", with: "Test pancake"
-    fill_in "Step", with: "Cook Test Pancake"
+    fill_in "Name", with: "Pancakes"
+    fill_in "Ingredients", with: "Flour\nEgg"
+    fill_in "Steps", with: "Mix\nMake"
     click_on("Create Recipe")
     visit recipes_path
-    expect(page).to have_content("Test Pancakes")
-    expect(page).to have_content(8)
+    expect(page).to have_content("Pancakes")
   end
 end
 
