756f61dabf217847772516be729520f737ed4981
Myles Moylan <moylanm@oregonstate.edu>
2019-04-27 13:03:01 -0700

    back-tracking some to sort shit out

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -1,12 +1,4 @@
 class Ingredient < ApplicationRecord
   belongs_to :recipe
 
-  def set_ingredient( new_ingredient )
-    self.ingredient = new_ingredient
-  end
-
-  def done?
-    return true if ingredient.present?
-  end
-
 end
--- app/models/step.rb
+++ app/models/step.rb
@@ -1,12 +1,4 @@
 class Step < ApplicationRecord
   belongs_to :recipe
 
-  def set_step( new_description )
-    self.step = new_description
-  end
-
-  def done?
-    return true if step.present?
-  end
-
 end
