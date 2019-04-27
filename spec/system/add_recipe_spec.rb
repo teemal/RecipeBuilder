b983be5c06b09e1dce958ad0f2a908e8c560943a
Myles Moylan <moylanm@oregonstate.edu>
2019-04-27 14:14:10 -0700

    add test for missing name

--- spec/system/add_recipe_spec.rb
+++ spec/system/add_recipe_spec.rb
@@ -1,14 +1,26 @@
 require "rails_helper"
 
 RSpec.describe "adding a recipe", type: :system do
-  it "allows a user to create a recipe with ingredients/steps" do
+  it "allows a user to create a recipe with name/ingredients/steps" do
     visit new_recipe_path
     fill_in "Name", with: "Pancakes"
     fill_in "Ingredients", with: "Flour\nEgg"
     fill_in "Steps", with: "Mix\nMake"
     click_on("Create Recipe")
     visit recipes_path
-    expect(page).to have_content("Pancakes")
+    @recipe = Recipe.find_by(name: "Pancakes")
+    expect(page).to have_selector(
+      "#recipe_#{@recipe.id} .name", text: "Pancakes"
+    )
+  end
+
+  it "does not allow a user to create a recipe without a name" do
+    visit new_recipe_path
+    fill_in "Name", with: ""
+    fill_in "Ingredients", with: "Flour\nEgg"
+    fill_in "Steps", with: "Mix\nMake"
+    click_on("Create Recipe")
+    expect(page).to have_selector(".new_recipe")
   end
 end
 
