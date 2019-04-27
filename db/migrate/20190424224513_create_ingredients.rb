8d86af668126d6b7ea5f40205cd8d27fe8c1eb79
Myles Moylan <moylanm@oregonstate.edu>
2019-04-27 15:00:40 -0700

    Step.step > description; Ingredient.ingredient > name

--- db/migrate/20190424224513_create_ingredients.rb
+++ db/migrate/20190424224513_create_ingredients.rb
@@ -2,7 +2,7 @@ class CreateIngredients < ActiveRecord::Migration[5.2]
   def change
     create_table :ingredients do |t|
       t.references :recipe, foreign_key: true
-      t.string :ingredient
+      t.string :name
 
       t.timestamps
     end
--- db/migrate/20190424224537_create_steps.rb
+++ db/migrate/20190424224537_create_steps.rb
@@ -2,7 +2,7 @@ class CreateSteps < ActiveRecord::Migration[5.2]
   def change
     create_table :steps do |t|
       t.references :recipe, foreign_key: true
-      t.string :step
+      t.string :description
 
       t.timestamps
     end
