a32ddc94d8a62e6f57c8c9c5c77893784d06b7a3
Myles Moylan <moylanm@oregonstate.edu>
2019-04-27 17:11:09 -0700

    add Ingredient tests

--- spec/models/ingredient_spec.rb
+++ spec/models/ingredient_spec.rb
@@ -1,21 +1,40 @@
 require "rails_helper"
+
 RSpec.describe Ingredient do
+  let(:test_ingredient) { "Flour" }
 
-  it "expects an Ingredient to have an ingredient" do
-    ingredient = Ingredient.new({ingredient: "Test Ingredient"})
-    expect(ingredient.ingredient).to eq("Test Ingredient")
+  describe "initialization" do
+    it "creates an ingredient given a name" do
+      ingredient = Ingredient.new(name: test_ingredient)
+      expect(ingredient.name).to eq(test_ingredient)
+    end
   end
 
-  it "expects an Ingredients to be able to change its name" do
-    new_ingredient = "New Test Ingredient"
+  it "expects to have an ingredient" do
     ingredient = Ingredient.new
-    ingredient.set_ingredient(new_ingredient)
-    expect(ingredient.ingredient).to eq(new_ingredient)
+    expect(ingredient.save).to be_falsy
   end
 
-  it "expects an Ingredient to be done when it has both a amount and a name" do 
-    ingredient = Ingredient.new({ingredient: "Test Ingredient"})
-    expect(ingredient.done?).to be_truthy
+  it "expects to be able to change its name" do
+    new_ingredient = "Sugar"
+    ingredient = Ingredient.new(name: test_ingredient)
+    ingredient.name = new_ingredient
+    expect(ingredient.name).to eq(new_ingredient)
   end
 
+  it "expects to be able to change its amount" do
+    new_amount = 100.0
+    ingredient = Ingredient.new(name: test_ingredient)
+    ingredient.amount = new_amount
+    expect(ingredient.amount).to eq(new_amount)
+  end
+
+  it "expects to be able set whether or not acquired" do
+    ingredient = Ingredient.new(name: test_ingredient)
+    expect(ingredient.acquired?).to be_falsy
+    ingredient.set_acquired
+    expect(ingredient.acquired?).to be_truthy
+    ingredient.unset_acquired
+    expect(ingredient.acquired?).to be_falsy
+  end
 end
