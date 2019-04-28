1a738337898fe05bd711886244cc358ca56f7e8d
Myles Moylan <moylanm@oregonstate.edu>
2019-04-28 12:09:09 -0700

    add methods for (un)setting completed attribute

--- app/models/step.rb
+++ app/models/step.rb
@@ -1,11 +1,21 @@
 class Step < ApplicationRecord
   before_save :default_values
+
   validates :description, presence: true
+  validates :completed, presence: true
 
   belongs_to :recipe
 
   def completed?
     completed
+  end
+
+  def set_completed
+    self.completed = true
+  end
+
+  def unset_completed
+    self.completed = false
   end
 
   private
