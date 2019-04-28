18c67f6d09ca66c850f597cb4e477e4d28d2d15a
Myles Moylan <moylanm@oregonstate.edu>
2019-04-27 17:01:50 -0700

    add amount columnt to ingredients table

--- db/migrate/20190424224513_create_ingredients.rb
+++ db/migrate/20190424224513_create_ingredients.rb
@@ -3,6 +3,7 @@ class CreateIngredients < ActiveRecord::Migration[5.2]
     create_table :ingredients do |t|
       t.references :recipe, foreign_key: true
       t.string :name
+      t.decimal :amount, precision: 7, scale: 1
       t.boolean :acquired
 
       t.timestamps
