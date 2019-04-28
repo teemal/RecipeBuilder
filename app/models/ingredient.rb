b6d765cbfc8e65552c5bf9fe6a2ab52c2ab0d7e3
Myles Moylan <moylanm@oregonstate.edu>
2019-04-27 17:10:46 -0700

    add amount and acquire state change methods

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -1,17 +1,28 @@
 class Ingredient < ApplicationRecord
   before_save :default_values
+
   validates :name, presence: true
+  validates :amount, presence: true
+  validates :acquired, presence: true
 
   belongs_to :recipe
 
-
   def acquired?
     acquired
+  end
+
+  def set_acquired
+    self.acquired = true
+  end
+
+  def unset_acquired
+    self.acquired = false
   end
 
   private
 
   def default_values
     self.acquired = acquired.presence || false
+    self.amount = amount.presense || 0.0
   end
 end
