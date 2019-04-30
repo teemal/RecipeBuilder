ef18af9b5df1bb4a505c81a305396e811390acf4
Myles Moylan <moylanm@oregonstate.edu>
2019-04-30 10:28:45 -0700

    add number attribute to step migration

--- db/migrate/20190424224537_create_steps.rb
+++ db/migrate/20190424224537_create_steps.rb
@@ -3,6 +3,7 @@ class CreateSteps < ActiveRecord::Migration[5.2]
     create_table :steps do |t|
       t.references :recipe, foreign_key: true
       t.string :description
+      t.integer :number
       t.boolean :completed
 
       t.timestamps
