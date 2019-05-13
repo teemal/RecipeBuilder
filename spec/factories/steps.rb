d49f1be694d7f6f397aec3aeaf1dfacc226d2763
Myles Moylan <moylanm@oregonstate.edu>
2019-05-13 09:22:42 -0700

    add custom factory objects

--- spec/factories/steps.rb
+++ spec/factories/steps.rb
@@ -1,7 +1,18 @@
-
 FactoryBot.define do
   factory :step do
     description { "Shake and Bake" }
+    completed { false }
+    recipe
+  end
+
+  factory :complete_step, class: Step do
+    description { "Salt" }
+    completed { true }
+    recipe
+  end
+
+  factory :incomplete_step, class: Step do
+    description { "Salt" }
     completed { false }
     recipe
   end
