2cf65bd2ed45cadd1309a698824933ff05318d72
Myles Moylan <moylanm@oregonstate.edu>
2019-05-13 09:10:16 -0700

    add custom factory objects

--- spec/factories/ingredients.rb
+++ spec/factories/ingredients.rb
@@ -1,6 +1,16 @@
 FactoryBot.define do
   factory :ingredient do
-    name { "Ingredient Name" }
+    name { 'Ingredient Name' }
+    acquired { false }
+  end
+
+  factory :complete_ingredient, class: Ingredient do
+    name { 'Complete Ingredient' }
+    acquired { true }
+  end
+
+  factory :incomplete_ingredient, class: Ingredient do
+    name { 'Incomplete Ingredient' }
     acquired { false }
   end
 end
