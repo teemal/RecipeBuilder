a73c03acf0d36d462a2e99d5ef8cd6211abf95e5
Myles Moylan <moylanm@oregonstate.edu>
2019-05-13 09:02:12 -0700

    recipe/ingredient spec work

--- spec/models/ingredient_spec.rb
+++ spec/models/ingredient_spec.rb
@@ -1,33 +1,38 @@
 require "rails_helper"
 
 RSpec.describe Ingredient do
-  let(:test_ingredient) { "Flour" }
+  let(:test_name) { "Flour" }
 
   describe "initialization" do
+    let(:recipe) { FactoryBot.build_stubbed(:recipe) }
+    let(:ingredient_with_name) { FactoryBot.build_stubbed(:ingredient, name: test_name) }
+    let(:ingredient_without_name) { FactoryBot.build_stubbed(:ingredient) }
+
     it "creates an ingredient given a name" do
-      ingredient = Ingredient.new(name: test_ingredient)
-      expect(ingredient.name).to eq(test_ingredient)
+      expect(ingredient_with_name.valid?).to be_truthy
+    end
+
+    it "fails if not given a name" do
+      expect(ingredient_without_name.valid?).to be_falsy
     end
   end
-
-  it "expects to have an ingredient" do
-    ingredient = Ingredient.new
-    expect(ingredient.save).to be_falsy
+/
+  describe "mutation" do
+    it "expects to be able to change its name" do
+      new_ingredient = "Sugar"
+      ingredient = Ingredient.new(name: test_ingredient)
+      ingredient.name = new_ingredient
+      expect(ingredient.name).to eq(new_ingredient)
+    end
+  
+    it "expects to be able set whether or not acquired" do
+      ingredient = Ingredient.new(name: test_ingredient)
+      expect(ingredient.acquired?).to be_falsy
+      ingredient.set_acquired
+      expect(ingredient.acquired?).to be_truthy
+      ingredient.unset_acquired
+      expect(ingredient.acquired?).to be_falsy
+    end
   end
-
-  it "expects to be able to change its name" do
-    new_ingredient = "Sugar"
-    ingredient = Ingredient.new(name: test_ingredient)
-    ingredient.name = new_ingredient
-    expect(ingredient.name).to eq(new_ingredient)
-  end
-
-  it "expects to be able set whether or not acquired" do
-    ingredient = Ingredient.new(name: test_ingredient)
-    expect(ingredient.acquired?).to be_falsy
-    ingredient.set_acquired
-    expect(ingredient.acquired?).to be_truthy
-    ingredient.unset_acquired
-    expect(ingredient.acquired?).to be_falsy
-  end
+  /
 end
--- spec/models/recipe_spec.rb
+++ spec/models/recipe_spec.rb
@@ -2,59 +2,64 @@ require "rails_helper"
 
 RSpec.describe Recipe do
   let(:test_name) { "Pancakes" }
+  let(:test_cuisine) { "Breakfast" }
 
   describe "initialization" do
+    let(:recipe_with_name) { FactoryBot.build_stubbed(:recipe, name: :test_name) }
+    let(:recipe_without_name) { FactoryBot.build_stubbed(:recipe) }
+
     it "creates a recipe given a name" do
-      recipe = Recipe.new(name: test_name)
-      expect(recipe.name).to eq(test_name)
+      expect(recipe_with_name.valid?).to be_truthy
+    end
+
+    it "fails if not given a name" do
+      expect(recipe_without_name.valid?).to be_falsy
     end
   end
 
-  it "expects to have a name" do
-    recipe = Recipe.new
-    expect(recipe.save).to be_falsy
-  end
+  describe "mutation" do
+    let(:recipe) { FactoryBot.build_stubbed(:recipe, 
+                                            name: test_name,
+                                            cuisine: test_cuisine) }
 
-  it "expects the name to be changeable" do
-    new_name = "Toast"
-    recipe = Recipe.new(name: test_name)
-    recipe.name = new_name
-    expect(recipe.name).to eq(new_name)
-  end
+    it "expects the name to be changeable" do
+      expect(recipe.name).to eq(test_name)
+      new_name = "Spagetti"
+      recipe.name = new_name
+      expect(recipe.name).to eq(new_name)
+    end
 
-  it "expects cuisine to be optional" do
-    recipe = Recipe.new(name: test_name)
-    expect(recipe.save).to be_truthy
-  end
-
-  it "expects cuisine to be changeable" do
-    recipe = Recipe.new(name: test_name)
-    expect(recipe.cuisine).to eq("Unknown")
-    recipe.cuisine = "French"
-    expect(recipe.cuisine).to eq("French")
+    it "expects cuisine to be changeable" do
+      expect(recipe.cuisine).to eq(test_cuisine)
+      new_cuisine = "Italian"
+      recipe.cuisine = new_cuisine
+      expect(recipe.cuisine).to eq(new_cuisine)
+    end
   end
 
   describe "with ingredients" do
-    let(:recipe) { Recipe.new(name: test_name) }
-    let(:ingredient) { Ingredient.new(name: "Bananas") }
+    let(:recipe) { FactoryBot.build(:recipe, 
+                                            name: test_name,
+                                            cuisine: test_cuisine) }
+    let(:complete_ingredient) { FactoryBot.build(:complete_ingredient) }
+    let(:incomplete_ingredient) { FactoryBot.build(:incomplete_ingredient) }
 
     it "expects that a recipe with incomplete ingredients is not submittable" do
-      recipe.ingredients << Ingredient.new
+      recipe.ingredients << incomplete_ingredient
       expect(recipe.submittable?).to be_falsy
     end
 
     it "expects that a recipe with all ingredients acquired is submittable" do
-      ingredient.set_acquired
-      recipe.ingredients << ingredient
+      recipe.ingredients << complete_ingredient
       expect(recipe.submittable?).to be_truthy
     end
 
     it "expects to be able to get a count of ingredients" do
-      recipe.ingredients << ingredient << ingredient
+      recipe.ingredients << complete_ingredient << incomplete_ingredient
       expect(recipe.ingredient_count).to eq(2)
     end
   end
-
+  /
   describe "with steps" do
     let(:recipe) { Recipe.new(name: test_name) }
     let(:step) { Step.new(description: "Shake and bake.") }
@@ -75,4 +80,5 @@ RSpec.describe Recipe do
       expect(recipe).to have_num_steps(2)
     end
   end
+  /
 end
