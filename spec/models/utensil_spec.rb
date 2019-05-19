10b66e1533ebd2afefc24582d4b8374146da65ee
mwinb <mwinb>
2019-05-18 20:20:40 -0700

    Replicated perform test on utensil with fake, mock, stub, and verified mock (double). commented out original test with BigDependency

--- spec/models/utensil_spec.rb
+++ spec/models/utensil_spec.rb
@@ -38,13 +38,35 @@ RSpec.describe "Utensil" do
     expect(utensil.completed?).to be_falsy
   end
 
+=begin
   it "expects to be able to perfrom" do
     result = utensil.perform(BigDependency.new)
     expect(result).to eq(42)
   end
+=end
 
   it "expects to be able to perfrom with a fake" do
     result = utensil.perform(FakeBigDependency.new)
+    expect(result).to eq(42)
+  end
+
+  it "expects to be able to perform with a stub" do
+    dependency_stub = double("execute": nil)
+    result = utensil.perform(dependency_stub)
+    expect(result).to eq(42)
+  end
+
+  it "expects to be able to perform with a double" do
+    dependency_double = double("BigDependency")
+    expect(dependency_double).to receive(:execute).and_return(nil)
+    result = utensil.perform(dependency_double)
+    expect(result).to eq(42)
+  end
+
+  it "expects to be able to perform with a verified double" do
+    dependency_double = instance_double("BigDependency")
+    expect(dependency_double).to receive(:execute).and_return(nil)
+    result = utensil.perform(dependency_double)
     expect(result).to eq(42)
   end
 
