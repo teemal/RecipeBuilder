5019ae54a36871cfb00a5d40d8ef88da5096b380
teemal <taymal1987@gmail.com>
2019-05-27 13:45:01 -0700

    add_recipe_spec: remove name checking in system test

--- spec/system/add_recipe_spec.rb
+++ spec/system/add_recipe_spec.rb
@@ -16,13 +16,5 @@ RSpec.describe "adding a recipe", type: :system do
     )
   end
 
-  it "does not allow a user to create a recipe without a name" do
-    visit new_recipe_path
-    fill_in "Name", with: ""
-    fill_in "Ingredients", with: "Flour\nEgg"
-    fill_in "Steps", with: "Mix\nMake"
-    click_on("Create Recipe")
-    expect(page).to have_selector(".new_recipe")
-  end
 end
 
